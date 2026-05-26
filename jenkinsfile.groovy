fb4d4b8ae85bd8d5d95a9541aff2617bca919ad3
santhoshchariengineer <santhoshchariengineer@gmail.com>
2026-05-26 11:00:53 +0530

    Add Jenkins pipeline for code fetching and validation

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,1 +1,28 @@
-
+pipeline{
+  agent any
+  stages{
+    stage('fetch code from SCM'){
+      steps{
+        echo'successfully fetched content from SCM'
+      }
+    }
+    
+    stage('Validate contect from SCM'){
+      steps{
+        if(fileExists(index.html){
+          sh grep -q '</html>' index.html
+          echo'validation successful'
+        } else {
+          error'validation failed'
+        }
+           }
+           }
+           }
+           
+    Stage('deploy code to nginx'){
+           sh 'cp index.html /usr/share/nginx/html'
+           echo'deployment success'
+    }
+           
+  }
+}
